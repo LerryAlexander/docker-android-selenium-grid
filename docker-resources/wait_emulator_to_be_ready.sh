f97b0b88b89f048ea7533c9845c4956fcaacd42d
LerryAlexander <lerry.ortiz@gmail.com>
2021-01-09 00:30:08 -0300

    update docker-resources/wait_emulator_to_be_ready.sh

--- docker-resources/wait_emulator_to_be_ready.sh
+++ docker-resources/wait_emulator_to_be_ready.sh
@@ -8,9 +8,21 @@
     docker --version
     docker exec $DOCKER_EMULATOR_SAMSUNG_S6 adb devices
     docker exec $DOCKER_EMULATOR_SAMSUNG_S10 adb devices
+    echo "hace adb kill-server en samsung 6"
+    docker exec $DOCKER_EMULATOR_SAMSUNG_S6 adb kill-server
+    echo "hace adb kill-server en samsung 10"
+    docker exec $DOCKER_EMULATOR_SAMSUNG_S10 adb kill-server
+    echo "hace adb start-server en samsung 6"
+    docker exec $DOCKER_EMULATOR_SAMSUNG_S6 adb start-server
+    echo "hace adb start-server en samsung 10"
+    docker exec $DOCKER_EMULATOR_SAMSUNG_S10 adb start-server
+    echo "hace adb get-state en samsung 6"
+    docker exec $DOCKER_EMULATOR_SAMSUNG_S6 adb get-state
+    echo "hace adb get-state en samsung 10"
+    docker exec $DOCKER_EMULATOR_SAMSUNG_S10 adb get-state
     #echo "validate with DOCKER_EMULATOR_SAMSUNG_S6"
     #docker exec $DOCKER_EMULATOR_SAMSUNG_S6 adb wait-for-device
-    echo "validate with DOCKER_EMULATOR_SAMSUNG_S10"
+    echo "hace wait-for-device en samsung 10"
     docker exec $DOCKER_EMULATOR_SAMSUNG_S10 adb wait-for-device shell getprop sys.boot_completed | tr -d '\r'
     statusS6="1"
     # $(docker exec "$DOCKER_EMULATOR_SAMSUNG_S6" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')

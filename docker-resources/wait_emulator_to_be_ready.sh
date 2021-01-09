664b310c335cc7859661e71b1eb26499e1bcdca7
LerryAlexander <lerry.ortiz@gmail.com>
2021-01-08 22:16:34 -0300

    update docker-resources/wait_emulator_to_be_ready.sh

--- docker-resources/wait_emulator_to_be_ready.sh
+++ docker-resources/wait_emulator_to_be_ready.sh
@@ -3,7 +3,9 @@
   boot_completed=false
   while [ "$boot_completed" == false ]; do
     echo "validate estatus s6"
-    statusS6=$(docker exec "$DOCKER_EMULATOR_SAMSUNG_S6" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
+    echo $DOCKER_EMULATOR_SAMSUNG_S6
+    echo $DOCKER_EMULATOR_SAMSUNG_S10
+    #statusS6=$(docker exec "$DOCKER_EMULATOR_SAMSUNG_S6" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
     echo "validate estatus s10"
     statusS10=$(docker exec "$DOCKER_EMULATOR_SAMSUNG_S10" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
     if [ "$statusS6" == "1" ] && [ "$statusS10" == "1" ]; then

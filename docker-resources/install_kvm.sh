77b91b58cd0782a37752a1d1e6a991eb41b55884
Lerry.Salazar <Lerry.Salazar@care.com>
2021-01-12 01:09:02 -0300

    update

---
+++ docker-resources/install_kvm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Installing KVM on "$DOCKER_EMULATOR_SAMSUNG_S6
+docker exec -ti $DOCKER_EMULATOR_SAMSUNG_S6 sh -c "apt-get update -y
+apt-get install qemu -y
+qemu-system-x86_64
+qemu-system-x86_64 \
+  -append 'root=/dev/vda console=ttyS0' \
+  -drive file='rootfs.ext2.qcow2,if=virtio,format=qcow2'  \
+  -enable-kvm \
+  -kernel 'bzImage' \
+  -nographic \
+;"
+
+echo "Installing KVM on "$DOCKER_EMULATOR_SAMSUNG_S10
+docker exec -ti $DOCKER_EMULATOR_SAMSUNG_S10 sh -c "apt-get update -y
+apt-get install qemu -y
+qemu-system-x86_64
+qemu-system-x86_64 \
+  -append 'root=/dev/vda console=ttyS0' \
+  -drive file='rootfs.ext2.qcow2,if=virtio,format=qcow2'  \
+  -enable-kvm \
+  -kernel 'bzImage' \
+  -nographic \
+;"

39145927824454adfe699abe58e984d6a186b65b
LerryAlexander <lerry.ortiz@gmail.com>
2021-01-12 21:09:36 -0300

    update docker-resources/check_emulator_container.sh

--- docker-resources/check_emulator_container.sh
+++ docker-resources/check_emulator_container.sh
@@ -2,7 +2,9 @@
 
   healthy_status=false
   docker exec $DOCKER_EMULATOR_SAMSUNG_S6 /root/emulator/emulator -avd Samsung Galaxy S6
+  docker exec $DOCKER_EMULATOR_SAMSUNG_S6 /root/emulator/emulator -list-avds
   docker exec $DOCKER_EMULATOR_SAMSUNG_S10 /root/emulator/emulator -avd Samsung Galaxy S10
+  docker exec $DOCKER_EMULATOR_SAMSUNG_S10 /root/emulator/emulator -list-avds
   while [ "$healthy_status" == false ]; do
     statusS6=$(docker inspect --format='{{json .State.Health}}' samsung_s6_container | grep -n '"Status":"healthy"')
     statusS10=$(docker inspect --format='{{json .State.Health}}' samsung_s10_container | grep -n '"Status":"healthy"')

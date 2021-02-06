1588614fbf5e319eca3ba0f66f416e50987da05c
LerryAlexander <lerry.ortiz@gmail.com>
2021-02-05 22:30:51 -0300

    update

--- docker-resources/check_emulator_container.sh
+++ docker-resources/check_emulator_container.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
   healthy_status=false
-  docker exec samsung_s6_container /root/emulator/emulator -avd samsung_galaxy_s6_6.0
-  docker exec samsung_s6_container /root/emulator/emulator -list-avds
-  docker exec samsung_s10_container /root/emulator/emulator -avd samsung_galaxy_s10_8.0
-  docker exec samsung_s10_container /root/emulator/emulator -list-avds
+  #docker exec samsung_s6_container /root/emulator/emulator -avd samsung_galaxy_s6_6.0
+  #docker exec samsung_s6_container /root/emulator/emulator -list-avds
+  #docker exec samsung_s10_container /root/emulator/emulator -avd samsung_galaxy_s10_8.0
+  #docker exec samsung_s10_container /root/emulator/emulator -list-avds
+  docker exec google_emulator /root/emulator/emulator -list-avds
   while [ "$healthy_status" == false ]; do
-    statusS6=$(docker inspect --format='{{json .State.Health}}' samsung_s6_container | grep -n '"Status":"healthy"')
-    statusS10=$(docker inspect --format='{{json .State.Health}}' samsung_s10_container | grep -n '"Status":"healthy"')
+    #statusS6=$(docker inspect --format='{{json .State.Health}}' samsung_s6_container | grep -n '"Status":"healthy"')
+    #statusS10=$(docker inspect --format='{{json .State.Health}}' samsung_s10_container | grep -n '"Status":"healthy"')
+    googleEmulator=$(docker inspect --format='{{json .State.Health}}' google_emulator | grep -n '"Status":"healthy"')
     if [ "$statusS6" != "" ] && [ "$statusS10" != "" ]; then
       healthy_status=true
       echo "emulators are ready"

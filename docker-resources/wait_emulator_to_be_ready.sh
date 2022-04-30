2277d302d3ed389add70e30336d4ac374ac0c81c
Lerry <lerry.ortiz@thinkific.com>
2022-04-30 02:21:06 -0400

    replacing samsung s10 by nexus 5

--- docker-resources/wait_emulator_to_be_ready.sh
+++ docker-resources/wait_emulator_to_be_ready.sh
@@ -3,8 +3,8 @@
   boot_completed=false
   while [ "$boot_completed" == false ]; do
     statusS6=$(docker exec "$DOCKER_EMULATOR_SAMSUNG_S6" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
-    statusS10=$(docker exec "$DOCKER_EMULATOR_SAMSUNG_S10" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
-    if [ "$statusS6" == "1" ] && [ "$statusS10" == "1" ]; then
+    statusN5=$(docker exec "$DOCKER_EMULATOR_NEXUS_5" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
+    if [ "$statusS6" == "1" ] && [ "$statusN5" == "1" ]; then
       boot_completed=true
       echo "emulators are ready"
       echo "executing tests..."

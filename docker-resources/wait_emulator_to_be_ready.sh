286092ef9187f15b0d58cf8fcc33904b1d4ece90
Lerry <lerry.ortiz@thinkific.com>
2022-04-30 02:33:28 -0400

    replacing nexus 5 by nexus 4 with android version 7.0

--- docker-resources/wait_emulator_to_be_ready.sh
+++ docker-resources/wait_emulator_to_be_ready.sh
@@ -3,8 +3,8 @@
   boot_completed=false
   while [ "$boot_completed" == false ]; do
     statusS6=$(docker exec "$DOCKER_EMULATOR_SAMSUNG_S6" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
-    statusN5=$(docker exec "$DOCKER_EMULATOR_NEXUS_5" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
-    if [ "$statusS6" == "1" ] && [ "$statusN5" == "1" ]; then
+    statusN4=$(docker exec "$DOCKER_EMULATOR_NEXUS_4" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
+    if [ "$statusS6" == "1" ] && [ "$statusN4" == "1" ]; then
       boot_completed=true
       echo "emulators are ready"
       echo "executing tests..."

55416a8e598f1eff7bf769b0f502b2264bb27314
Lerry <lerry.ortiz@thinkific.com>
2022-05-01 01:40:06 -0400

    refactoring mobile capabilities

--- src/test/java/utilities/Hooks.java
+++ src/test/java/utilities/Hooks.java
@@ -30,8 +30,8 @@ public class Hooks {
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("platformName", driverCapabilities.getPlatform());
         caps.setCapability("platformVersion", driverCapabilities.getPlatformVersion());
-        //caps.setCapability("avd", driverCapabilities.getDeviceName());
-        caps.setCapability("appium:uiautomator2ServerInstallTimeout", 60000);
+        caps.setCapability("browserName", driverCapabilities.getDeviceName());
+        //caps.setCapability("appium:uiautomator2ServerInstallTimeout", 60000);
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("app", Constants.APP_CONTAINER_PATH);
         caps.setCapability("noSign", true);

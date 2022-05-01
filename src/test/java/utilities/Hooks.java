9777bd502d2fed1e1edffab723cf4d9b68a07e96
Lerry <lerry.ortiz@thinkific.com>
2022-05-01 00:43:20 -0400

    Not passing the emulator name

--- src/test/java/utilities/Hooks.java
+++ src/test/java/utilities/Hooks.java
@@ -32,7 +32,7 @@ public class Hooks {
         caps.setCapability("platformVersion", driverCapabilities.getPlatformVersion());
         caps.setCapability("appPackage", "io.cloudgrey.the_app");
         caps.setCapability("appActivity", "io.cloudgrey.the_app.MainActivity");
-        caps.setCapability("deviceName", driverCapabilities.getDeviceName());
+        //caps.setCapability("deviceName", driverCapabilities.getDeviceName());
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("app", Constants.APP_CONTAINER_PATH);
         caps.setCapability("noSign", true);

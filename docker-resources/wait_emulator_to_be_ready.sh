2da08bb286f552ffe58dad67f837ff64daf1d981
Lerry <lerry.ortiz@thinkific.com>
2022-05-03 00:24:50 -0400

    adding check_emulator_popups during booting process to avoid error 'UI System isn't responding'

--- docker-resources/wait_emulator_to_be_ready.sh
+++ docker-resources/wait_emulator_to_be_ready.sh
@@ -7,6 +7,9 @@
     if [ "$statusPhone" == "1" ] && [ "$statusTablet" == "1" ]; then
       boot_completed=true
       echo "devices are ready !!"
+      echo "checking if there is a popup showed up in any of the emulators"
+      docker exec "$DOCKER_EMULATOR_PHONE" ./check_amulator_popups.sh
+      docker exec "$DOCKER_EMULATOR_TABLET" ./check_amulator_popups.sh
       echo "executing tests..."
     else
       echo "waiting for devices to be ready"

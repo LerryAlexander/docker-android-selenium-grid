6f4de8d95587bb842ce811cd3a3c6ee417c5dcc8
Lerry <lerry.ortiz@thinkific.com>
2022-05-03 13:30:03 -0400

    wait 10 sec before executing tests

--- docker-resources/wait_emulator_to_be_ready.sh
+++ docker-resources/wait_emulator_to_be_ready.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+function wait_emulator_to_be_ready(){
   boot_completed=false
   while [ "$boot_completed" == false ]; do
     statusPhone=$(docker exec "$DOCKER_EMULATOR_PHONE" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
     statusTablet=$(docker exec "$DOCKER_EMULATOR_TABLET" adb wait-for-device shell getprop sys.boot_completed | tr -d '\r')
     if [ "$statusPhone" == "1" ] && [ "$statusTablet" == "1" ]; then
       boot_completed=true
+      sleep 10
       echo "devices are ready !!"
-      echo "checking if there is a popup showed up in any of the emulators"
-      docker exec "$DOCKER_EMULATOR_PHONE" ./check_amulator_popups.sh
-      docker exec "$DOCKER_EMULATOR_TABLET" ./check_amulator_popups.sh
       echo "executing tests..."
     else
       echo "waiting for devices to be ready"
       sleep 1
     fi
   done
+}

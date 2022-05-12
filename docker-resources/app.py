e7f95d84337b58375ef0f46b508cb302889eea1d
Lerry <lerry.ortiz@thinkific.com>
2022-05-12 01:16:21 -0400

    refactoring cmd command

--- docker-resources/app.py
+++ docker-resources/app.py
@@ -172,7 +172,7 @@ def appium_run(avd_name: str):
             if not os.path.isdir(jar_url):
                 os.mkdir(jar_url)
             start_selenium_node = "/opt/bin/start-selenium-grid-node-docker.sh"
-            cmd += ' & timeout 10 bash -c "until printf \"\" 2>>/dev/null >>/dev/tcp/{appium_host}/{appium_port}; do sleep 1; done; echo appium is ready and listening on port {appium_port}; wget -O /opt/selenium/selenium-server.jar {selenium_jar_url} && bash {start_node}"'.format(appium_host=appium_host, appium_port=appium_port, selenium_jar_url=download_selenium_server_url, start_node=start_selenium_node)
+            cmd += " & timeout 10 bash -c 'until printf \"\" 2>>/dev/null >>/dev/tcp/{appium_host}/{appium_port}; do sleep 1; done; echo appium is ready and listening on port {appium_port}; wget -O /opt/selenium/selenium-server.jar {selenium_jar_url} && bash {start_node}'".format(appium_host=appium_host, appium_port=appium_port, selenium_jar_url=download_selenium_server_url, start_node=start_selenium_node)
         except ValueError as v_err:
             logger.error(v_err)
     else:
@@ -215,7 +215,8 @@ def create_node_config_selenium_grid_4(avd_name: str, appium_host: str, appium_p
         os.mkdir(file_dir)
     with open(output_file, "w") as toml_file:
         toml_string = toml.dump(config, toml_file)
-    logger.info('Appium node config: {config}'.format(config=toml_string))
+    logger.info('Appium node config: '
+                '{config}'.format(config=toml_string))
 
 def create_node_config(avd_name: str, browser_name: str, appium_host: str, appium_port: int, selenium_host: str,
                        selenium_port: int, selenium_timeout: int, selenium_proxy_class: str):

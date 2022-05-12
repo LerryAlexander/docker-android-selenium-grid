221c12c068d7e78b13a47b51c847aaef3c89a3d7
Lerry <lerry.ortiz@thinkific.com>
2022-05-11 20:46:38 -0400

    adding apt-get update

--- docker-resources/run_app.sh
+++ docker-resources/run_app.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 curl -o src/get-pip.py https://bootstrap.pypa.io/pip/3.6/get-pip.py -o get-pip.py \
-&& apt-get install python3-distutils -y \
+&& apt-get update \
 && apt-get install python3-apt \
-&& python3 get-pip.py \
+&& apt-get install python3.6-distutils -y \
+&& python3 src/get-pip.py \
 && pip install toml \
 && python3 -m src.app
